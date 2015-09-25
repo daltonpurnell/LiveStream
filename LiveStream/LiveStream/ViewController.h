bcfd5c33edccaed0ebd7470f884b42c0ce8d83ce
daltonpurnell <daltonpurnell@live.com>
2015-09-25 13:16:36 -0600

    initial commit

---
+++ LiveStream/LiveStream/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LiveStream
+//
+//  Created by Dalton on 9/25/15.
+//  Copyright © 2015 Dalton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LiveStream/LiveStream/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  LiveStream
+//
+//  Created by Dalton on 9/25/15.
+//  Copyright © 2015 Dalton. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
